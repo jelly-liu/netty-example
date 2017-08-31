171eafd50d08ebc6c212fc06913cef99fecd25e3
jelly <collonn@126.com>
2017-08-31 15:53:08 +0800

    init

---
+++ src/main/java/com/jelly/openProject/App.java
@@ -0,0 +1,13 @@
+package com.jelly.openProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
